2e4697e63efffe2452adc8cf827d006efa223016
james-wag <james.lane@wagwalking.com>
2024-05-08 11:45:25 -0600

    Hack dependencies into build

---
+++ MapboxCoreSearch.xcframework/ios-arm64_x86_64-simulator/MapboxCoreSearch.framework/Headers/MBXSResultAccuracy.h
@@ -0,0 +1,30 @@
+// This file is generated and will be overwritten automatically.
+
+#import <Foundation/Foundation.h>
+
+/**
+ * A point accuracy metric for the returned search result.
+ * \sa https://docs.mapbox.com/api/search/geocoding/#point-accuracy-for-address-features
+ */
+// NOLINTNEXTLINE(modernize-use-using)
+typedef NS_ENUM(NSInteger, MBXSResultAccuracy)
+{
+    /** Result is a known address point but has no specific accuracy. */
+    MBXSResultAccuracyPoint,
+    /** Result is for a specific building/entrance. */
+    MBXSResultAccuracyRooftop,
+    /** Result is derived from a parcel centroid. */
+    MBXSResultAccuracyParcel,
+    /** Result has been interpolated from an address range. */
+    MBXSResultAccuracyInterpolated,
+    /** Result is for a block or intersection. */
+    MBXSResultAccuracyIntersection,
+    /** Result is an approximate location. */
+    MBXSResultAccuracyApproximate,
+    /** Result is a street centroid. */
+    MBXSResultAccuracyStreet,
+    /** Result is a known address point but does not intersect a known rooftop/parcel. */
+    MBXSResultAccuracyProximate
+} NS_SWIFT_NAME(ResultAccuracy);
+
+NSString* MBXSResultAccuracyToString(MBXSResultAccuracy result_accuracy);
